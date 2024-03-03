373cc9e1f04a724076cd32981009d9877b8ec4b3
adlzanchetta <adlzanchetta@gmail.com>
2024-03-03 15:21:59 -0500

    Fixes 'baseUrl' config attribute

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,15 +13,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://adlzanchetta.github.io',
+  baseUrl: '/my/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'adlzanchetta', // GitHub user name.
+  projectName: 'my',                // Usually repository name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
